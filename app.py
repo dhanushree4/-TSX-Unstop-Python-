598b6d62b16474f257b22b6d02fac3c21f956587
DHANUSHREE <163735796+dhanushree4@users.noreply.github.com>
2025-05-15 16:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
